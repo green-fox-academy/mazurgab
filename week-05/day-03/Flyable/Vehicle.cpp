fd96ad5fded113d9847f251034e58cc647f5958e
mazurgab <mazurgab@gmail.com>
2018-11-21 16:35:54 +0100

    Added Vehicle class

---
+++ week-05/day-03/Flyable/Vehicle.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Mazúr Gábor on 2018. 11. 21..
+//
+
+#include "Vehicle.h"
---
+++ week-05/day-03/Flyable/Vehicle.h
@@ -0,0 +1,16 @@
+#ifndef FLYABLE_VEHICLE_H
+#define FLYABLE_VEHICLE_H
+
+#include <iostream>
+
+
+class Vehicle {
+protected:
+    std::string car;
+    std::string bus;
+    std::string train;
+
+};
+
+
+#endif //FLYABLE_VEHICLE_H
