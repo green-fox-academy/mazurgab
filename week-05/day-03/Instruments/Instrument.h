02cce89918ad5236c55a64a4d3a3a275fbc04967
mazurgab <mazurgab@gmail.com>
2018-11-21 14:19:11 +0100

    Added instrument class to exec. Instruments

---
+++ week-05/day-03/Instruments/Instrument.h
@@ -0,0 +1,15 @@
+#ifndef INSTRUMENTS_INSTRUMENT_H
+#define INSTRUMENTS_INSTRUMENT_H
+
+#include <string>
+
+class Instrument {
+public:
+    virtual void play() = 0;
+
+protected:
+    std::string _name;
+};
+
+
+#endif //INSTRUMENTS_INSTRUMENT_H
