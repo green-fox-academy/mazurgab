638d083486684a0e30750d66fda32ad7057a4d01
mazurgab <mazurgab@gmail.com>
2019-01-02 17:13:23 +0100

    Fixed Computer exercise(typedef)

--- week-07/day-02/Computer/main.c
+++ week-07/day-02/Computer/main.c
@@ -4,28 +4,28 @@
 // Use the Computer struct, give values to the fields and print them out in the main!
 // Use the Notebook struct, give values to the fields and print them out in the main!
 
-struct Computer {
+typedef struct Computer {
     float cpu_speed_GHz;
     int ram_size_GB;
     int bits;
-};
+}Computer_t;
 
-typedef struct notebook{
+typedef struct Notebook{
     float cpu_speed_GHz;
     int ram_size_GB;
     int bits;
-} notebook_t;
+} Notebook_t;
 
 int main()
 {
 
-    struct Computer computer;
+    Computer_t computer;
     computer.cpu_speed_GHz = 2.66f;
     computer.ram_size_GB = 8;
     computer.bits = 64;
     printf("The computer's CPU size is: %f, RAM size: %d, bits: %d\n", computer.cpu_speed_GHz, computer.ram_size_GB, computer. bits);
 
-    notebook_t notebook1;
+    Notebook_t notebook1;
     notebook1.cpu_speed_GHz = 2.66f;
     notebook1.ram_size_GB = 8;
     notebook1.bits = 64;
