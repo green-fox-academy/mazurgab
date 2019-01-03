64ae19c4a00a0e595222b39b8ef4db80b3c45063
mazurgab <mazurgab@gmail.com>
2019-01-03 13:23:19 +0100

    Finished Sentence exercise

--- week-07/day-03/Sentence/main.c
+++ week-07/day-03/Sentence/main.c
@@ -5,17 +5,20 @@
 // and splits a string to words by space
 // solve the problem with the proper string.h function
 
-void split(char array);
+void split(char array[]);
 
 int main()
 {
-    printf("Type in a sentence:\n\n");
-    char string[256];
+    char string[] = {"You wanna sail this ship against the winds by lighting a fire under her deck? I have no time for such a nonsense."};
+    split(string);
 
     return(0);
 }
 
-void split(char array){
-
+void split(char array[]){
+    for (char *p = strtok(array, " "); p != NULL; p = strtok(NULL, " "))
+    {
+        puts(p);
+    }
 
 }
