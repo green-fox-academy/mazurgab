6277b1acfce73b88d8dcaf6eaa263138811d9f26
mazurgab <mazurgab@gmail.com>
2019-04-16 18:16:21 +0200

    Refactored AppendA exercise

--- week-02/day-01/AppendA/main.cpp
+++ week-02/day-01/AppendA/main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char* args[]) {
     std::string animals[] = {"koal", "pand", "zebr"};
 
     for (int i=0; i<3; i++) {
-        std::cout << "a" << animals[i] << std::endl;
+        std::cout << animals[i] << "a" <<std::endl;
     }
     // - Create an array variable named `animals`
     //   with the following content: `["koal", "pand", "zebr"]`
