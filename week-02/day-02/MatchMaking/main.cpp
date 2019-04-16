8f155d3721be5e0f00251e682e5c2b4d0231827a
mazurgab <mazurgab@gmail.com>
2019-04-16 23:20:53 +0200

    Refactored MatchMaking exercise

--- week-02/day-02/MatchMaking/main.cpp
+++ week-02/day-02/MatchMaking/main.cpp
@@ -20,48 +20,26 @@ int main(int argc, char* args[])
 
     return 0;
 }
-
+/*
 std::vector<std::string> makingMatches(const std::vector<std::string>& girls, const std::vector<std::string>& boys)
 {
 
     std::vector<std::string> couples(boys);
     int j=1;
-    for (int i = 0; i < boys.size()-1; ++i)
+    for (int i = 0; i < girls.size(); ++i)
     {
         couples.insert((couples.begin()+j),girls[i]);
         j += 2;
     }
     return couples;
-}
+}*/
 
-/*
-#include <iostream>
-#include <string>
-#include <vector>
-
-std::vector<std::string> makingMatches(const std::vector<std::string>& girls, const std::vector<std::string>& boys);
-
-int main(int argc, char* args[])
-{
-    std::vector<std::string> girls = {"Eve","Ashley","Claire","Kat","Jane"};
-    std::vector<std::string> boys = {"Joe","Fred","Tom","Todd","Neef","Jeff"};
-
-    // Write a method that joins the two lists by matching one girl with one boy into a new list
-    // Exepected output: "Eve", "Joe", "Ashley", "Fred"...
-
-    for(const auto& planet : makingMatches(girls, boys))
-    {
-        std::cout << planet << " ";
-    }
-
-    return 0;
-}
 
 std::vector<std::string> makingMatches(const std::vector<std::string>& girls, const std::vector<std::string>& boys)
 {
     std::vector<std::string> list = {};
     int bigger = 0;
-    bigger = (boys.size() < girls.size() ? girls : boys).size(); - ha igaz: ? ..., ha nem igaz: : ...
+    bigger = (boys.size() < girls.size() ? girls : boys).size();
     for (int i = 0; i < bigger; i++)
     {
         if(i < boys.size()) {
@@ -74,4 +52,3 @@ std::vector<std::string> makingMatches(const std::vector<std::string>& girls, co
     return list;
 }
 
-*/
