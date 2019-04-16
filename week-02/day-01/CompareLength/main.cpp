465d3d5bcd7a6bfdd1aeb0bc92e1e90fbe1429c6
mazurgab <mazurgab@gmail.com>
2019-04-16 18:24:09 +0200

    Refactored CompareLength exercise

--- week-02/day-01/CompareLength/main.cpp
+++ week-02/day-01/CompareLength/main.cpp
@@ -6,9 +6,11 @@ int main(int argc, char* args[]) {
     int p1 [] = {1,2,3};
     int p2 [] = {4,5};
 
-    if (sizeof(p2)> sizeof(p1)) {
+    if(sizeof(p2)> sizeof(p1)) {
         std::cout << "`p2` has more elements than `p1`" << std::endl;
-    } else {
+    }else if(sizeof(p2) == sizeof(p1)){
+        std::cout << "`p1` has the same number elements than `p2`" << std::endl;
+    }else{
         std::cout << "`p1` has more elements than `p2`" << std::endl;
     }
 
