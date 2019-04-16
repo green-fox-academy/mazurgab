25b9f656e93c9ee536c1e0d361394a7925b7f6a3
mazurgab <mazurgab@gmail.com>
2019-04-16 23:36:49 +0200

    Refactored Reverse exercise

--- week-02/day-02/Reserve/main.cpp
+++ week-02/day-02/Reserve/main.cpp
@@ -1,29 +1,26 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
 
 std::string reverse(const std::string& text);
 
 int main(int argc, char* args[])
 {
-    std::string reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI";
-    std::string nextText = reversed;
+    std::string text = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI";
 
     // Create a method that can reverse an std:string, which is passed as the parameter
     // Use it on this reversed string to check it!
     // Try to solve this using .at() first, and optionally anything else after.
     // Hint: You might use a temporary variable to swap 2 characters or you can use std::swap function.
 
-    std::cout << reverse(nextText) << std::endl;
+    std::cout << reverse(text) << std::endl;
 
     return 0;
 }
 
 std::string reverse(const std::string& text)
 {
-    std::string reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI";
-    std::string nextText = reversed;
-    for(int i = 0; i<= nextText.size(); i++){
-        nextText[i] = reversed[reversed.size()-i];
-    }
-    return nextText;
+    std::string reversed = text;
+    std::reverse(reversed.begin(), reversed.end());
+    return reversed;
 }
