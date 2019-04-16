0d20b3943a6469a6a16bbcd7427a1e249d41d4bf
mazurgab <mazurgab@gmail.com>
2019-04-16 18:28:50 +0200

    Refactored DoubleItems exercise

--- week-02/day-01/DoubleItems/main.cpp
+++ week-02/day-01/DoubleItems/main.cpp
@@ -3,9 +3,9 @@
 
 int main(int argc, char* args[]) {
 
-    int numList[] = {3, 4, 5, 6, 7};
+    int numList[] = {3, 4, 5, 6, 7, 8};
 
-    for (int i=0; i<5; i++){
+    for (int i=0; i<(sizeof(numList)/sizeof(numList[0])); i++){
 
         numList[i] = numList[i] * 2;
         std::cout<< numList[i] << std::endl;
