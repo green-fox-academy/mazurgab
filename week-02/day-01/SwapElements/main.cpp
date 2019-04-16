271a409cb4b96aa0d8bb4dbb40be7fafb81a0f05
mazurgab <mazurgab@gmail.com>
2019-04-16 19:10:13 +0200

    Refactored SwapElements exercise

--- week-02/day-01/SwapElements/main.cpp
+++ week-02/day-01/SwapElements/main.cpp
@@ -6,19 +6,12 @@ int main(int argc, char* args[]) {
 
     std::string abc[] = {"first", "second", "third"};
 
-    std::string d;
-
-    d = abc[0];
-    abc[0] = abc[2];
-    abc[2] = d;
+    std::swap(abc[0], abc[2]);
 
     for (int i=0; i<3; i++) {
 
-
         std::cout << abc[i] << std::endl;
     }
-
-
 
     // - Create an array variable named `abc`
     //   with the following content: `["first", "second", "third"]`
