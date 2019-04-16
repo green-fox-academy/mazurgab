2b3225c0490ae72ed0fdc73af62f9a5d39bdfdd1
mazurgab <mazurgab@gmail.com>
2019-04-16 19:08:15 +0200

    Refactored Summing exercise

--- week-02/day-01/Summing/main.cpp
+++ week-02/day-01/Summing/main.cpp
@@ -1,9 +1,6 @@
 #include <iostream>
-#include <string>
-
 
 int sum(int x);
-
 
 int main(int argc, char* args[]) {
 
@@ -12,16 +9,10 @@ int main(int argc, char* args[]) {
     std::cout<<"Give a number"<<std::endl;
     std::cin>>number;
 
-    int value = sum(number);
-
-    std::cout<<"The result is: "<<value<<std::endl;
-
-
+    std::cout<<"The result is: "<<sum(number)<<std::endl;
 
     // - Write a function called `sum` that sum all the numbers
     //   until the given parameter and returns with an integer
-
-
 
     return 0;
 }
@@ -31,9 +22,7 @@ int sum(int x){
     int result = 0;
 
     for (int i=0; i<=x; i++){
-
         result += i;
-
     }
     return result;
 }
